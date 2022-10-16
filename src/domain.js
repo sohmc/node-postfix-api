cfad5074a168df330c6d5c2b19679139d10ea58d
Michael Soh <2767724+sohmc@users.noreply.github.com>
2022-10-16 13:50:18 +0000

    Added linting and made linting changes.

--- src/domain.js
+++ src/domain.js
@@ -1,33 +1,38 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
 
 exports.handler = async (event) => {
   // (optional) fetch method and querystring
   const method = event.requestContext.http.method;
   const queryParam = event.queryStringParameters;
   console.log(`Received ${method} request with ${queryParam}`);
-  
-  // retrieve signature and payload
-  const webhookSignature = event.headers;
-  const webhookPayload = JSON.parse(event.body);
 
-  console.log(`Received Headers: ` + JSON.stringify(webhookSignature));
-  console.log(`Received Payload: ` + JSON.stringify(webhookPayload));
-  
-  let statusCode = 900;
-  let returnBody = {"message": "Processing Input"};
+  // retrieve signature and payload
+  const webhookHeaders = event.headers;
+  const webhookBody = JSON.parse(event.body);
+
+  console.log('Received Headers: ' + JSON.stringify(webhookHeaders));
+  console.log('Received Body: ' + JSON.stringify(webhookBody));
+
+  const lambdaResponseObject = {
+    statusCode: 418,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: '{"message": "I am a teapot"}',
+  };
 
   try {
     if (method === 'GET') {
       if (Object.prototype.hasOwnProperty.call(queryParam, 'domain')) {
         getDomainInformation(queryParam.domain);
-        statusCode = 200;
-        returnBody = {"message": "This function is working!"};
+        lambdaResponseObject.statusCode = 200;
+        lambdaResponseObject.body = { 'message': 'This function is working!' };
       } else if (Object.prototype.hasOwnProperty.call(queryParam, 'q')) {
-        statusCode = 400;
-        returnBody = {"message": "This function has not been implemented"};
+        lambdaResponseObject.statusCode = 400;
+        lambdaResponseObject.body = { 'message': 'This function has not been implemented' };
       } else {
-        statusCode = 400;
-        returnBody = {"message": "This function has not been implemented"};
+        lambdaResponseObject.statusCode = 400;
+        lambdaResponseObject.body = { 'message': 'This function has not been implemented' };
       }
     }
 
@@ -36,17 +41,43 @@ exports.handler = async (event) => {
   } catch (error) {
     console.error(error);
     return {
-        statusCode: 400,
-        body: `Cannot process event: ${error}`,
+      statusCode: 400,
+      body: `Cannot process event: ${error}`,
     };
   }
 
-  return {
-    statusCode: statusCode, // default value
-    body: JSON.stringify(returnBody),
-  };
+  return lambdaResponseObject;
 };
 
 function getDomainInformation(domainIn) {
+  mysqlQuery(`SELECT * FROM domains WHERE domain='${domainIn}'`);
   return true;
+}
+
+function mysqlQuery(query, queryValues) {
+  let queryResults = [];
+
+  const dbConnection = mysql.createConnection({
+    host: process.env.POSTFIX_HOST,
+    user: process.env.POSTFIX_USER,
+    password: process.env.POSTFIX_PASSWORD,
+  });
+
+  dbConnection.query(query, queryValues, function(error, results) {
+    if (error) {
+      console.log(error);
+      queryResults = ['error', error];
+    } else if (results.length > 0) {
+      console.log('Results length: ' + results.length);
+      queryResults = results;
+    } else {
+      queryResults = ['error', 'No results found'];
+    }
+
+    console.log(results);
+  });
+
+  dbConnection.end();
+
+  return queryResults;
 }
--- src/index.js
+++ src/index.js
@@ -1,32 +1,15 @@
-exports.handler = async (event) => {
-  // (optional) fetch method and querystring
-  const method = event.requestContext.http.method;
-  const queryParam = event.queryStringParameters.myCustomParameter;
-  console.log(`Received ${method} request with ${queryParam}`);
-  
-  // retrieve signature and payload
-  const webhookSignature = event.headers;
-  const webhookPayload = JSON.parse(event.body);
+exports.handler = async (lambdaEvent, lambdaContext) => {
+  console.log('handler event: ' + JSON.stringify(lambdaEvent));
+  console.log('handler context: ' + JSON.stringify(lambdaContext));
 
-  console.log(`Received Headers: ` + JSON.stringify(webhookSignature));
-  console.log(`Received Payload: ` + JSON.stringify(webhookPayload));
-  
-  try {
-      // validateSignature(webhookSignature); // throws if invalid signature
-      // handleEvent(webhookPayload); // throws if processing error
-      true;
-  } catch (error) {
-      console.error(error);
-      return {
-          statusCode: 400,
-          body: `Cannot process event: ${error}`,
-      };
-  }
-
-  return {
-      statusCode: 200, // default value
-      body: JSON.stringify({
-          received: true,
-      }),
+  const lambdaResponseObject = {
+    statusCode: 418,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: '{"message": "I am a teapot"}',
   };
+
+
+  return lambdaResponseObject;
 };
