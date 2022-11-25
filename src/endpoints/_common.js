e2169ec6b26275bf6592f1174f9220a64081cc7d
Michael Soh <2767724+sohmc@users.noreply.github.com>
2022-11-25 16:40:12 +0000

    Starting work on POST -- UNTESTED

--- src/endpoints/_common.js
+++ src/endpoints/_common.js
@@ -1,7 +1,7 @@
 const { DynamoDB } = require('@aws-sdk/client-dynamodb');
 const client = new DynamoDB({ region: 'us-east-1' });
 
-const { DynamoDBDocument, GetCommand, QueryCommand } = require('@aws-sdk/lib-dynamodb');
+const { DynamoDBDocument, GetCommand, QueryCommand, PutCommand } = require('@aws-sdk/lib-dynamodb');
 const ddbDocClient = DynamoDBDocument.from(client);
 
 module.exports = {
@@ -90,4 +90,54 @@ module.exports = {
 
     return returnData;
   },
+  async putAliasItem(placeholderObject) {
+    console.log('common.js:putAliasItem -- placeholderObject: ' + JSON.stringify(placeholderObject));
+
+    const { v4: uuidv4 } = require('uuid');
+    const d = Date.now() / 1000;
+
+    const params = {
+      'TableName': process.env.POSTFIX_DYNAMODB_TABLE,
+      'Item': {
+        'application': 'postfix',
+        'domain': 'someDomain',
+        'alias_address': 'someAlias',
+        'destination': 'someDestination',
+        'full_address': '',
+        'uuid': uuidv4(),
+        'created': d,
+        'modified': d,
+        'active': true,
+        'ignore_alias': false,
+        'count': 1,
+      },
+      'ConditionExpression': 'attribute_not_exists(domain) AND attribute_not_exists(alias_address)',
+    };
+
+    console.log('ddbDocClient parameters: ' + JSON.stringify(params));
+    const data = await sendDocClientCommand(new PutCommand(params));
+    console.log('Received data: ', JSON.stringify(data));
+
+    let returnData = [];
+    if (Object.prototype.hasOwnProperty.call(data, 'Items')) returnData = data.Items;
+
+    return returnData;
+  },
 };
+
+
+async function sendDocClientCommand(commandPackage) {
+  console.log('common.js:sendDocClientCommand');
+
+  try {
+    const data = await ddbDocClient.send(commandPackage);
+    console.log('Received data: ', JSON.stringify(data));
+
+    let returnData = [];
+    if (Object.prototype.hasOwnProperty.call(data, 'Items')) returnData = data.Items;
+
+    return returnData;
+  } catch (err) {
+    console.error('DynamoDB returned an error: ' + err);
+  }
+}
