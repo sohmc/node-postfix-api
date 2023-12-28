6acb2eb8f655bbab34604ce094969fe124bf2d10
Michael Soh <2767724+sohmc@users.noreply.github.com>
2023-12-28 22:14:07 +0000

    Added insertAliasObject and checkDomainConfig as is

--- src/newEndpoints/alias/POST.js
+++ src/newEndpoints/alias/POST.js
@@ -1,18 +1,23 @@
-const allowedParameters = {
-  'alias': 'full_address',
-  'q': 'full_address',
-  'destination': 'destination',
-  'active': 'active_alias',
-  'ignore': 'ignore_alias',
-};
+import { execute as getDomainConfig } from '../domain/GET';
 
+const requiredParameters = ['alias', 'domain', 'destination']
 
-export async function execute(pathParameters = [], queryParameters = {}) {
-  const requiredProperties = ['alias', 'domain', 'destination'];
+export async function execute(pathParameters = [], queryParameters = {}, requestBody = {}) {
+  let lambdaResponseObject = {
+    statusCode: 418,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: {
+      'code': 418,
+      'type': 'domain',
+      'message': '',
+    },
+  };
 
-  if (requiredProperties.findIndex(property => Object.prototype.hasOwnProperty.call(requestBody, property)) === -1) {
+  if (requiredParameters.findIndex(property => Object.prototype.hasOwnProperty.call(requestBody, property)) === -1) {
     // return error message with property that is missing
-    lambdaResponseObject.body.message = 'missing required property: ' + requiredProperties[requiredProperties.findIndex(property => Object.prototype.hasOwnProperty.call(requestBody, property))];
+    lambdaResponseObject.body.message = 'missing required property: ' + requiredParameters[requiredParameters.findIndex(property => Object.prototype.hasOwnProperty.call(requestBody, property))];
   } else if (await checkDomainConfig(requestBody.domain) == -1) {
     // return error message stating domain is invalid
     lambdaResponseObject.body.message = 'domain is either inactive or invalid';
@@ -25,4 +30,43 @@ export async function execute(pathParameters = [], queryParameters = {}) {
 
     lambdaResponseObject = await insertAliasObject(aliasObject);
   }
+
+  return lambdaResponseObject;
+}
+
+async function insertAliasObject(placeholderObject) {
+  let returnObject = {
+    statusCode: 405,
+    body: {
+      'code': 405,
+      'type': 'alias',
+      'message': 'Could not insert alias.',
+    },
+  };
+
+  // PUT row into database
+  const queryResults = await commonFunctions.putAliasItem(placeholderObject);
+  if (Object.prototype.hasOwnProperty.call(queryResults, 'affectedRows') && (queryResults.affectedRows === 1)) {
+    // if everything was successful, get the domain information from the database and return it as a response.
+    const getAliasPlaceholdersObject = {
+      'alias_address': placeholderObject.alias_address,
+      'domain': placeholderObject.domain,
+    };
+    returnObject = await getAliasInformation(getAliasPlaceholdersObject);
+    returnObject.statusCode = 201;
+  } else {
+    returnObject.body.message = 'Alias could not be added.';
+  }
+
+  return returnObject;
+}
+
+
+async function checkDomainConfig(domainToCheck) {
+  const domainListResponse = await getDomainConfig([], {});
+  const domainList = JSON.parse(domainListResponse.body);
+
+  console.log(JSON.stringify(domainList));
+
+  return domainList.findIndex(domain => ((domain.subdomain == domainToCheck) && (domain.active)));
 }
