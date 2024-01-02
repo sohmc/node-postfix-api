1aa1a39db2bd4074a616277c9a10a9733510e20a
Michael Soh <2767724+sohmc@users.noreply.github.com>
2024-01-02 02:09:42 +0000

    Copied functions as-is to PATCH.js

--- src/newEndpoints/domain/PATCH.js
+++ src/newEndpoints/domain/PATCH.js
@@ -1,4 +1,5 @@
-
+import { updateItem } from '../_utilities';
+import { execute as domainGet } from './GET';
 
 const allowedProperties = ['description', 'active'];
 
@@ -17,12 +18,17 @@ export async function execute(pathParameters = [], queryParameters = {}, request
     },
   };
 
+  const placeholderObject = {
+    'domain': 'tacomail-config',
+    'alias_address': 'sub_domains',
+  };
+
   // If there is a path parameter and there is at least one accepted property in the request body, then continue
   if ((pathParameters.length > 0) && (allowedProperties.findIndex((property) => Object.prototype.hasOwnProperty.call(requestBody, property)) >= 0)) {
-    placeholderObject.subdomain = pathParameters[0].trim();
-    const currentConfig = await getDomainInformation(placeholderObject);
+    placeholderObject.searchingSubdomain = pathParameters[0].trim();
+    const currentConfig = await domainGet();
     console.log('domain.js:POST-CurrentConfig - ' + JSON.stringify(currentConfig));
-    const subDomainPosition = currentConfig.body.findIndex(element => element.subdomain == placeholderObject.subdomain);
+    const subDomainPosition = currentConfig.body.findIndex(element => element.subdomain == placeholderObject.searchingSubdomain);
 
     if (subDomainPosition == -1) {
       lambdaResponseObject.statusCode = 405;
@@ -45,4 +51,80 @@ export async function execute(pathParameters = [], queryParameters = {}, request
   }
 
   return lambdaResponseObject;
+}
+
+async function updateDomainConfigObject(placeholderObject, subDomainPosition) {
+  let returnObject = {
+    statusCode: 405,
+    body: {
+      'code': 405,
+      'type': 'domain',
+      'message': 'Domain could not be updated',
+    },
+  };
+
+  // Update the config ITEM
+  const queryResults = await updateDomainConfigItem(placeholderObject, subDomainPosition);
+  if (Object.prototype.hasOwnProperty.call(queryResults, 'affectedRows') && (queryResults.affectedRows === 1)) {
+    // if everything was successful, get the domain information from the database and return it as a response.
+    returnObject = await getDomainInformation(placeholderObject, { 'sub_domain': placeholderObject.subdomain });
+  } else {
+    returnObject.body.type = 'domain';
+    returnObject.body.message = 'error updating domain in configuration';
+  }
+
+  return returnObject;
+}
+
+async function updateDomainConfigItem(placeholderObject, subDomainPosition) {
+  console.log('common.js:updateDomainConfigItem -- placeholderObject: ' + JSON.stringify(placeholderObject));
+  const d = Math.floor(Date.now() / 1000);
+
+  const params = {
+    'TableName': process.env.POSTFIX_DYNAMODB_TABLE,
+    'Key': {
+      'alias_address': placeholderObject.alias_address,
+      'sub_domain': placeholderObject.domain,
+    },
+    // You can't use an array (e.g. configValues[1]) here.  It must be explicitly defined
+    'ExpressionAttributeNames': {
+      '#kn1': 'configValues',
+    },
+    'ExpressionAttributeValues': {
+      ':yf1': placeholderObject.subdomain,
+      ':yf2': d,
+    },
+    'ConditionExpression': `#kn1[${subDomainPosition}].subdomain = :yf1`,
+  };
+
+  const setArray = [`#kn1[${subDomainPosition}].modified_datetime = :yf2`];
+  for (let index = 0; index < Object.keys(placeholderObject).length; index++) {
+    const property = Object.keys(placeholderObject)[index];
+    const placeholderName = 'pt' + index;
+
+    // skip keys since they are already declared
+    switch (property) {
+    case 'domain':
+    case 'alias_address':
+      continue;
+
+    default:
+      setArray.push(`#kn1[${subDomainPosition}].#${placeholderName} = :${placeholderName}`);
+      break;
+    }
+
+    console.log('common.js:updateDomainConfigItem -- adding property ' + property + '=' + placeholderObject[property]);
+    params.ExpressionAttributeNames[`#${placeholderName}`] = property;
+    params.ExpressionAttributeValues[`:${placeholderName}`] = placeholderObject[property];
+  }
+
+  // Create UpdateExpression
+  params.UpdateExpression = 'SET ' + setArray.join(', ');
+  console.log('common.js:updateDomainConfigItem -- ddbDocClient parameters: ' + JSON.stringify(params));
+  const data = await sendDocClientCommand(new UpdateCommand(params));
+  console.log('common.js:updateDomainConfigItem -- Received data: ', JSON.stringify(data));
+
+  if (Object.prototype.hasOwnProperty.call(data, '$metadata') && (data['$metadata'].httpStatusCode !== 200)) return [];
+
+  return { 'affectedRows': 1, 'Item': placeholderObject };
 }
