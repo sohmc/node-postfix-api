ce6f6f738a101ccf543f648c6b4683d49948db5a
Michael Soh <2767724+sohmc@users.noreply.github.com>
2024-01-03 02:14:50 +0000

    updated domain names for get tests

--- src/tests/domain-get.test.js
+++ src/tests/domain-get.test.js
@@ -14,7 +14,7 @@ test('Get current domain config', async () => {
   const result = await handler(lambdaEvent, {});
 
   expect(result.statusCode).toEqual(200);
-  expect(result).toHaveProperty('body');
+  expect(Array.isArray(result.body)).toBeTruthy();
 });
 
 test('Get config for specific domain', async () => {
@@ -22,7 +22,7 @@ test('Get config for specific domain', async () => {
     'requestContext': {
       'http': {
         'method': 'GET',
-        'path': '/domain/capricasp0vcmueb5h.ga',
+        'path': '/domain/capricadev.tk',
       },
     },
   };
@@ -32,7 +32,7 @@ test('Get config for specific domain', async () => {
   expect(result.statusCode).toEqual(200);
   expect(Array.isArray(result.body)).toBeTruthy();
   expect(result.body).toHaveLength(1);
-  expect(result.body[0].subdomain).toBe('capricasp0vcmueb5h.ga');
+  expect(result.body[0].subdomain).toBe('capricadev.tk');
 });
 
 test('Do a query for domains', async () => {
@@ -43,7 +43,7 @@ test('Do a query for domains', async () => {
         'path': '/domain',
       },
       'queryStringParameters': {
-        'q': '.ga',
+        'q': '.tk',
       },
     },
   };
